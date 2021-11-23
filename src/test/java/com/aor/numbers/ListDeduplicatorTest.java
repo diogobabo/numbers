7bb9a6357deed5931d0912ddb88b40531200ca31
DiogoBabo <diogo.babo@gmail.com>
2021-11-23 14:11:59 +0000

    sda

---
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -0,0 +1,50 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDeduplicatorTest {
+
+    @Test
+    public void deduplicate() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        class StubListSorter implements GenericListSorter{
+            @Override public List<Integer> sort(List<Integer> list) {
+                return  Arrays.asList(1, 2, 4, 5);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
+        List<Integer> distinct = deduplicator.deduplicate(list);
+        Assertions.assertEquals(1, distinct);
+    }
+    @Test
+    public void bug_deduplicate_8726() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        class StubListSorter implements GenericListSorter{
+            @Override public List<Integer> sort(List<Integer> list) {
+                return  Arrays.asList(1, 2, 2, 4);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
+        List<Integer> distinct = deduplicator.deduplicate(list);
+        Assertions.assertEquals(3, distinct);
+    }
+    @Test
+    public void max_bug_8726() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        class StubListDeduplicator implements GenericListDeduplicator{
+            @Override public List<Integer> deduplicate(List<Integer> list) {
+                return  Arrays.asList(1, 2, 4);
+            }
+        }
+        ListAggregator aggregator = new ListAggregator();
+        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        int distinct = aggregator.distinct(list, deduplicator);
+        Assertions.assertEquals(3, distinct);
+    }
+}
